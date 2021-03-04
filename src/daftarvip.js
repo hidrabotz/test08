be3e1b043bcc5ec7860feef66e7a14ec499249fa
hidrabotz <hidrabotz@gmail.com>
2021-03-04 13:27:28 -0400

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-R$. 10 > Acessar recursos ViP
+-R$. 20 > Recursos VIP + Insira o bot no seu grupo!
+-R$. 100 > Bot e seu você altera o comando que quiser, o bot é seu!
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5567996005372 ou digite *${prefix}dono*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+_https://chat.whatsapp.com/IeSragZuQhZ9Tt1R4A0adt `
+}
+exports.daftarvip = daftarvip
